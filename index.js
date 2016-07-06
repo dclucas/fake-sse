2d3d1f700ffff10f3cdac4649420c31d59778289
dclucas <diogo.lucas@gmail.com>
2016-07-06 16:06:06 -0300

    adds loop choice (true/false) and event name as API input.

--- index.js
+++ index.js
@@ -17,50 +17,24 @@ server.register([require('inert'), require('susie')], (err) => {
         method: 'POST',
         path: '/fakes',
         handler: function (request, reply) {
-            console.log(request.payload);
+            const payload = request.payload;
             server.route({
                 method: request.payload.method,
                 path: request.payload.path,
-                handler: function(req, res) { 
+                handler: function(req, res) {
                     const stream = new PassThrough({ objectMode: true });
-                    const l = request.payload.data.length;
+                    const l = payload.data.length;
                     var i = 0;
-                    setInterval(() => {
-                        //stream.write({ name: 'BDGRS', price: (500 + Math.floor(Math.random() * 100)).toString(), order: Math.floor(Math.random() * 2) === 1 ? 'BUY' : 'SELL' });
-                        //stream.write({ name: 'MSHRM', price: (120 + Math.floor(Math.random() * 200)).toString(), order: Math.floor(Math.random() * 2) === 1 ? 'BUY' : 'SELL' });
-                        //stream.write({ name: 'ASNKE', price: (900 + Math.floor(Math.random() * 50)).toString(), order: Math.floor(Math.random() * 2) === 1 ? 'BUY' : 'SELL' });
-                        stream.write(request.payload.data[i++ % l]);
+                    var timer = setInterval(() => {
+                        stream.write(payload.data[i++ % l]);
+                        if ((i >= l) && !payload.repeat) {
+                            clearInterval(timer);
+                        }
                     }, 200);
-                    res.event(stream, null, { event: 'stock' });
+                    res.event(stream, null, { event: payload.event });
                 }
             });
             reply('done');
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/{p*}',
-        handler: {
-            directory: {
-                path: Path.join(__dirname, 'public')
-            }
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/stocks',
-        handler: function (request, reply) {
-
-            const stream = new PassThrough({ objectMode: true });
-            setInterval(() => {
-
-                stream.write({ name: 'BDGRS', price: (500 + Math.floor(Math.random() * 100)).toString(), order: Math.floor(Math.random() * 2) === 1 ? 'BUY' : 'SELL' });
-                stream.write({ name: 'MSHRM', price: (120 + Math.floor(Math.random() * 200)).toString(), order: Math.floor(Math.random() * 2) === 1 ? 'BUY' : 'SELL' });
-                stream.write({ name: 'ASNKE', price: (900 + Math.floor(Math.random() * 50)).toString(), order: Math.floor(Math.random() * 2) === 1 ? 'BUY' : 'SELL' });
-            }, 200);
-            reply.event(stream, null, { event: 'stock' });
         }
     });
 
